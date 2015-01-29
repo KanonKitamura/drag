f3edfcf9ceecd600e9b8e37fe8a0eb8406d59273
KanonKitamura <kanonkitamura0405@gmail.com>
2015-01-29 21:58:56 +0900

    Initial Commit

---
+++ drag/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  drag
+//
+//  Created by kanon kitamura on 2015/01/29.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ drag/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  drag
+//
+//  Created by kanon kitamura on 2015/01/29.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
